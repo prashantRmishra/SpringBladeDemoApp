c8742cf1a48a45bd8994b848767c05b0eb20e921
prashantrmishra <pm7044872@gmail.com>
2020-06-10 12:06:46 +0530

    main file changed

--- demoproj/src/main/java/com/example/DemoApplication.java
+++ demoproj/src/main/java/com/example/DemoApplication.java
@@ -3,6 +3,7 @@ import com.blade.Blade;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+	
          Blade.of().get("/home", ctx -> ctx.text("Home Page")).start(DemoApplication.class,args);
 	}
 
